9f9f4f61178f8b62fe12567fae9d7e1ccdcf5bf8
Manish Kumawat <111861277+manish70945656555@users.noreply.github.com>
2023-09-13 07:10:20 +0530

    Update training_pipeline.py

--- src/pipelines/training_pipeline.py
+++ src/pipelines/training_pipeline.py
@@ -1,21 +1,37 @@
 import os
 import sys
-from src.logger import logging
-from src.exception import CustomException
 import pandas as pd
 
+from src.logger import logging
+from src.exception import CustomException
 from src.components.data_ingestion import DataIngestion
 from src.components.data_transformation import DataTransformation
 from src.components.model_trainer import ModelTrainer
 
-if __name__=='__main__':
-    obj=DataIngestion()
-    train_data_path,test_data_path=obj.initiate_data_ingestion()
-    print(train_data_path,test_data_path)
-    
-    data_transforamtion=DataTransformation()
-    
-    train_arr,test_arr,preprocessor_path = data_transforamtion.initiate_data_transformation(train_data_path,test_data_path)
-    
-    model_trainer=ModelTrainer()
-    model_trainer.initiate_model_training(train_arr,test_arr)
+def main():
+    # Data Ingestion
+    train_data_path, test_data_path = perform_data_ingestion()
+    print(train_data_path, test_data_path)
+
+    # Data Transformation
+    train_arr, test_arr, preprocessor_path = perform_data_transformation(train_data_path, test_data_path)
+
+    # Model Training
+    perform_model_training(train_arr, test_arr)
+
+def perform_data_ingestion():
+    data_ingestion = DataIngestion()
+    train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
+    return train_data_path, test_data_path
+
+def perform_data_transformation(train_data_path, test_data_path):
+    data_transformation = DataTransformation()
+    train_arr, test_arr, preprocessor_path = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+    return train_arr, test_arr, preprocessor_path
+
+def perform_model_training(train_arr, test_arr):
+    model_trainer = ModelTrainer()
+    model_trainer.initiate_model_training(train_arr, test_arr)
+
+if __name__ == '__main__':
+    main()
